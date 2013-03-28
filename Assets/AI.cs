6db858208241bbdc25d85e440da84b08a3e229e1
Alex Johnson <johnsonalex1@me.com>
2013-03-28 11:05:28 -0400

    AI.cs Asset

---
+++ Assets/AI.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+	
+	public int x;
+	public int y;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
